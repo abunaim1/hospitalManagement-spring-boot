64835b43a9f56b5924da1b1f86230f589f1e0eda
Abu Naim <aabunnaim@gmail.com>
2026-03-01 14:34:57 +0600

    impletem appointment and insurance

---
+++ src/main/java/com/hospitalManagement/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,29 @@
+package com.hospitalManagement.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", nullable = false) // Patient is required and not nullable
+    private Patient patient;
+
+}
